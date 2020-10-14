676f5d937473017580cfebdd542fb3f9e2b7ebae
Paulo Mellin Gimenes <paulo.gimenes@runrun.it>
2020-10-14 18:24:08 -0300

    App basic layout - work in progress

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import styles from './styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MaxStream</Text>
-      <StatusBar style="auto" />
+      <View style={styles.quarterHeight} />
+      <View style={styles.halfHeight} />
+      <View style={[styles.quarterHeight, { backgroundColor: '#CCC' }]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/App/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+      flex: 1,
+      flexDirection: 'column'
+  },
+  halfHeight: {
+      flex: .9,
+      backgroundColor: 'red'
+  },
+  quarterHeight: {
+      flex: .25,
+      backgroundColor: 'blue'
+  }
+});
